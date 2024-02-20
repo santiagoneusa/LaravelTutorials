144fb0160af8dfce2488f80546bc29a0696eab3e
santiagoneusa <sneusar@gmail.com>
2024-02-20 06:51:15 -0500

    Activity 2 in developing: Created the controler and the contact page view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+
+    public function contact(): View
+    {
+        return view('home.contact');
+    }
 }
