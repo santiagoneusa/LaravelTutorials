4c074a9b84d2355daed2cbb6908bffeb7747f3c0
santiagoneusa <sneusar@gmail.com>
2024-02-20 07:15:37 -0500

    Activity 2 Completed: View finished with the static information

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 @section('title', 'Contact - Online Store')
-@section('subtitle', 'Contact')
+@section('subtitle', 'Contact Us')
 @section('content')
-<!-- <div class="container">
-  <div class="row">
-    <div class="col-lg-4 ms-auto">
-      <p class="lead">{{ $description }}</p>
-    </div>
-    <div class="col-lg-4 me-auto">
-      <p class="lead">{{ $author }}</p>
-    </div>
-  </div>
-</div> -->
+<div class="container pt-3">
+
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <i class="fas fa-envelope mr-2"></i> Email: <a href="mailto:info@onlinestore.com">info@onlinestore.com</a>
+    </li>
+    <li class="list-group-item">
+      <i class="fas fa-map-marker-alt mr-2"></i> Address: 123 Main Street, Anytown, CA 90210
+    </li>
+    <li class="list-group-item">
+      <i class="fas fa-phone mr-2"></i> Phone: (555) 555-5555
+    </li>
+  </ul>
+
+</div>
 @endsection
