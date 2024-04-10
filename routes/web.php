6bccb412df945dcf9758e6ee816e5b11a4ed5b09
santiagoneusa <sneusar@gmail.com>
2024-04-09 21:19:23 -0500

    Implement API system for products

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,8 @@ Auth::routes();
 Route::get('api/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
 Route::get('api/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
 
-Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
-Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
+Route::get('api/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
+Route::get('api/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
+
+Route::get('api/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::get('api/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
